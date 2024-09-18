e81b3bcbd0a2314b0e0a8d539cc957f0ad290643
Faniloniaina RAMILISON <faniloniainaa21@gmail.com>
2024-09-18 16:22:20 +0300

    solve kangoroo

--- kangaroo.ts
+++ kangaroo.ts
@@ -1,12 +1,12 @@
 function kangaroo(x1: number, v1: number, x2: number, v2: number): string {
-    var sommeK1: number = x1
-    var sommeK2 : number = x2
-    for(var i = 0 ; i < 10 ; i++ ){
-        sommeK1 = v1 + sommeK1
-        sommeK2 = v2 + sommeK2
-        if(sommeK1 === sommeK2){
-            return "YES"
-        }
+  var sommeK1: number = x1;
+  var sommeK2: number = x2;
+  for (var i = 0; i < 10000000; i++) {
+    sommeK1 = v1 + sommeK1;
+    sommeK2 = v2 + sommeK2;
+    if (sommeK1 === sommeK2) {
+      return "YES";
     }
-    return "NO"
+  }
+  return "NO";
 }
